bf879fbe962e6db2de11df6d9c5518973f788ee5
arobu <arobu@arobu42.42heilbronn.de>
2023-11-27 14:30:45 -0600

    redone

--- srcs/requirements/nginx/tools/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes  \
-    -out $CERT \
-    -keyout $KEY \
-    -subj "/C=DE/ST=Heilbronn/L=Heilbronn/O=42 School/OU=arobu/CN=arobu/"
-
-nginx -g 'daemon off;'
